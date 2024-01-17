eade64eb3fd4c983f9fafb92cf8e7f747f12ac16
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-01-17 12:36:23 -0600

    added filler content with js

--- src/modules/addContent.js
+++ src/modules/addContent.js
@@ -3,5 +3,16 @@ export { addContent };
 const content = document.querySelector("#content");
 
 function addContent() {
-  content.textContent = "HELLO";
+  const h1 = document.createElement("h1");
+  const p = document.createElement("p");
+  const img = document.createElement("img");
+
+  h1.textContent = "The most exceptional restaurant";
+  p.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores libero autem enim provident, quae sit quasi dignissimos nihil harum aliquid cupiditate, architecto quibusdam? Voluptas quaerat quis vero magnam distinctio ipsum expedita nemo quibusdam deleniti, assumenda dolorem laudantium placeat impedit minima ab error fuga, dolor blanditiis rerum? Mollitia tenetur, non unde reprehenderit nam impedit iusto amet aliquam, exercitationem commodi cumque assumenda laudantium deserunt. Itaque saepe explicabo nam soluta animi quae optio.";
+  img.src = "https://placehold.co/600x400";
+
+  content.appendChild(h1);
+  content.appendChild(img);
+  content.appendChild(p);
 }
